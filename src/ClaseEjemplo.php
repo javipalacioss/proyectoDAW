d2b2a826ff9b1b696cdd9ba2b955c0d49e4e134e
javipalacioss <145489460+javipalacioss@users.noreply.github.com>
2025-05-13 20:47:32 +0200

    Add files via upload

---
+++ src/ClaseEjemplo.php
@@ -0,0 +1,16 @@
+<?php
+
+class ClaseEjemplo
+{
+    /**
+     * Realiza una suma de dos números.
+     *
+     * @param int|float $num1 El primer número.
+     * @param int|float $num2 El segundo número.
+     * @return int|float El resultado de la suma.
+     */
+    public function suma($num1, $num2)
+    {
+        return $num1 + $num2;
+    }
+}
